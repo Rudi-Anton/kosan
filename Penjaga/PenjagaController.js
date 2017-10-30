549e884a3317de933daef7e7ba479c055c551841
DESKTOP-IOJV8SG\RudiAnton <anttonms@gmail.com>
2017-10-30 15:45:46 +0700

    no message

--- Penjaga/PenjagaController.js
+++ Penjaga/PenjagaController.js
@@ -1,16 +1,32 @@
-Penjaga=require('./PenjagaModel.js');
-module.exports.getPenjaga=function(callback,limit){
-    Penjaga.find(callback).limit(limit);
+Penjaga = require('./PenjagaModel.js');
+module.exports.getPenjaga = function (callback, limit) {
+    Penjaga.aggregate([
+        {
+            "$lookup": {
+                from: "Kos",
+                localField: "KdKos",
+                foreignField: "KdKos",
+                as: "infoKos"
+            }
+        },
+        {
+            "$lookup": {
+                from: "GajiPenjaga",
+                localField: "KategoriKos",
+                foreignField: "KategoriKos",
+                as: "infoKategoriKos"
+            }
+        }])
 }
-module.exports.createPenjaga=function(akses,callback){
-    Penjaga.create(akses,callback);
+module.exports.createPenjaga = function (akses, callback) {
+    Penjaga.create(akses, callback);
 }
-module.exports.removePenjaga=function(_id,callback){
-    Penjaga.findByIdAndRemove(_id,callback);
+module.exports.removePenjaga = function (_id, callback) {
+    Penjaga.findByIdAndRemove(_id, callback);
 }
-module.exports.updatePenjaga=function(_id,akses,callback){
-    Penjaga.findByIdAndUpdate(_id,akses,callback);
+module.exports.updatePenjaga = function (_id, akses, callback) {
+    Penjaga.findByIdAndUpdate(_id, akses, callback);
 }
-module.exports.getPenjagaById=function(id,callback){
-    Penjaga.findById(id,callback);
+module.exports.getPenjagaById = function (id, callback) {
+    Penjaga.findById(id, callback);
 }
